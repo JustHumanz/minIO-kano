0caaa864cbbb5a311517eed242be7671582fc51f
Humanz <just@humanz.moe>
2022-02-09 20:27:48 +0700

    Update

--- src/minio.js
+++ src/minio.js
@@ -2,9 +2,9 @@ var Minio = require('minio')
 import Config from '../config.json'
 const buket = 'kano-bucket'
 var minioClient = new Minio.Client({
-    endPoint: Config.EndPoint,
-    port: 9000,
-    useSSL: false,
+    endPoint: Config.MinIO_EndPoint,
+    port: 443,
+    useSSL: true,
     accessKey: Config.accessKey,
     secretKey: Config.secretKey
 });
@@ -14,7 +14,7 @@ export function getBktList() {
     var data = []
     stream.on('data', function(obj) {
         data.push({
-            url:    Config.MinIOURL+"/"+buket+"/"+obj.name,
+            url:    "https://"+Config.MinIO_EndPoint+"/"+buket+"/"+obj.name,
             size:   obj.size,
             source: obj.metadata['X-Amz-Meta-Source'],
             author: atob(obj.metadata['X-Amz-Meta-Author'])
